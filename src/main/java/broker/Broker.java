b999eb1014035c920cad96679f0377567ad6208c
Yash Pandey <ypandey474@gmail.com>
2026-05-10 15:43:30 +0530

    CreateTopic method

--- src/main/java/broker/Broker.java
+++ src/main/java/broker/Broker.java
@@ -8,15 +8,30 @@ public class Broker {
     Map<String, Topic> topics;
     boolean autoTopicCreate;
     int defaultPartition;
-    public Broker (boolean autoTopicCreate , int defaultPartition){
-    this.topics =new HashMap<>();
-    this.defaultPartition=defaultPartition;
-    this.autoTopicCreate=autoTopicCreate;
+
+    public Broker(boolean autoTopicCreate, int defaultPartition) {
+        this.topics = new HashMap<>();
+        this.defaultPartition = defaultPartition;
+        this.autoTopicCreate = autoTopicCreate;
+    }
+    
+    public void createTopic(String topicName, int numPartitions) {
+        if (topics.containsKey(topicName)) {
+            System.out.printf("The topic %s is already present\n", topicName);
+        }
+        topics.put(topicName, new Topic(topicName, numPartitions));
+        System.out.printf("New topic created with name %s and partitions %d\n", topicName, numPartitions);
     }
 
 
     public void publishMessage(String message, String topicName) {
         // Given a message and topic, publish it
+        
+        if (!topics.containsKey(topicName)) {
+            System.out.printf("Topic with name %s does not exist and auto creation set to", topicName);
+            return;
+        }
+        
         Topic t = topics.get(topicName);
         t.publishMessage(message);
     }
--- src/main/java/broker/Main.java
+++ src/main/java/broker/Main.java
@@ -3,6 +3,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        bool automaticTopicCreation = false;
+
+        broker.createTopic("orders", 3);
+        broker.publishMessage("hello", "orders");
+
+        broker.publishMessage("hello1", "aldnasnd");
     }
     
 }
