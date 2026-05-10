892c6656bff004850cece1246c907fbcaa7efa73
Swapnil Kothari <swapnilkothari23@gmail.com>
2026-05-10 15:08:41 +0530

    crazzzzzyy

--- src/main/java/broker/Broker.java
+++ src/main/java/broker/Broker.java
@@ -1,9 +1,15 @@
 package KafkaClone.src.main.java.broker;
+import java.util.HashMap;
 import java.util.Map;
+
 
 // Broker is a kafka server, we start with only one server containing all topics
 public class Broker {
     Map<String, Topic> topics;
+    public Broker (){
+    this.topics =new HashMap<>();
+    }
+
 
     public void publishMessage(String message, String topicName) {
         // Given a message and topic, publish it
